53ed3e191f52c8ed1725da333e8f177210d775b5
Andrey Pavlov <couchemar@yandex.ru>
2015-05-31 17:54:30 +0000

    Используем hovercraft

--- default.nix
+++ default.nix
@@ -1,28 +1,11 @@
 let pkgs = import <nixpkgs> {}; in
 
 { stdenv ? pkgs.stdenv,
-  fetchurl ? pkgs.fetchurl,
-  pythonPackages ? pkgs.pythonPackages,
+  hovercraft ? pkgs.python34Packages.hovercraft,
   chromium ? pkgs.chromium,
   strings ? pkgs.lib.strings }:
 
 let
-  landslide = pythonPackages.buildPythonPackage rec {
-    name = "landslide-1.1.3";
-
-    src = fetchurl {
-      url = "https://pypi.python.org/packages/source/l/landslide/landslide-1.1.3.tar.gz";
-      md5 = "81d0e79bbc748c2fca90d2cbd5f85d06";
-    };
-
-    propagatedBuildInputs = with pythonPackages; [
-      jinja2
-      markdown
-      pygments
-      docutils
-      six
-    ];
-  };
 
   readAndIndentSources = file: strings.concatStringsSep "\n    " (
       strings.splitString "\n" (builtins.readFile file));
@@ -39,15 +22,9 @@ in
     my_math2_init_py = readAndIndentSources(./my_math2/my_math/__init__.py);
     my_math2_default_nix = readAndIndentSources(./my_math2/default.nix);
 
-    buildPhase = ''
-    substituteAll $src/slides.md slides.md
-    ${landslide}/bin/landslide --linenos=no --theme=ribbon slides.md
-    '';
-
     installPhase = ''
-    mkdir $out/
-    cp presentation.html $out/
-
-    echo "${chromium}/bin/chromium --incognito $out/presentation.html" >> $out/run
+    substituteAll slides.rst _slides.rst
+    ${hovercraft}/bin/hovercraft _slides.rst $out
+    echo "${chromium}/bin/chromium --incognito $out/index.html" >> $out/run
     '';
   }
